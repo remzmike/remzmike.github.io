6ba68a404e19b9ee4d6914e44d4602e1d0cf6e8e
remzmike <remzmike@gmail.com>
2021-06-15 15:12:31 -0400

    .

--- simpleui/m_v8.js
+++ simpleui/m_v8.js
@@ -5,31 +5,44 @@
 
 const m_v8_enabled = false;
 
-const m_v8 = {
-	IsSmi: (x) => {
-		if (!m_v8_enabled) return true;
+function m_v8_IsSmi(x) {
+	if (!m_v8_enabled) return true;
 
-        if (x != null && x != undefined && typeof(x) == 'number') { // idk, maybe prevents crashes
-			//return %IsSmi(x);			
-		} else {
-			return false;
-		}
-	},	
-	IsValidSmi: (x) => {
-		if (!m_v8_enabled) return true;
-
-        if (x != null && x != undefined && typeof(x) == 'number') { // idk, maybe prevents crashes
-			//return %IsValidSmi(x);			
-		} else {
-			return false;
-		}
-	},
-	//
-	assert_smi: (x) => {
-		if (!m_v8_enabled) return true;
-
-        console.assert(m_v8.IsSmi(x));
-		console.assert(m_v8.IsValidSmi(x));
+	if (x != null && x != undefined && typeof(x) == 'number') { // idk, maybe prevents crashes
+		//return %IsSmi(x);
+	} else {
+		return false;
 	}
-};
+}
 
+function m_v8_IsValidSmi(x) {
+	if (!m_v8_enabled) return true;
+
+	if (x != null && x != undefined && typeof(x) == 'number') { // idk, maybe prevents crashes
+		return true; //return %IsValidSmi(x); // is this gone now?
+	} else {
+		return false;
+	}
+}
+
+function m_v8_assert_smi(x) {
+	if (!m_v8_enabled) return true;
+	console.assert(m_v8_IsSmi(x));
+	console.assert(m_v8_IsValidSmi(x));
+}
+
+function m_v8_HasFastProperties(o) {
+	if (!m_v8_enabled) return true;
+	if (o != null) {
+		//return %HasFastProperties(o);
+	} else {
+		return false;
+	}
+}
+
+const m_v8 = {
+	IsSmi: m_v8_IsSmi,
+	IsValidSmi: m_v8_IsValidSmi,
+	assert_smi: m_v8_assert_smi,
+	HasFastProperties: m_v8_HasFastProperties,
+};
